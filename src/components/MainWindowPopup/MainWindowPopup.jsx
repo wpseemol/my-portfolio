4f636c3e60c9e294e935ef6c9ec568efad832b56
wpseemol <wpseemol@gmail.com>
2023-12-12 02:00:46 +0600

    profile icon add on my portfole website

--- src/components/MainWindowPopup/MainWindowPopup.jsx
+++ src/components/MainWindowPopup/MainWindowPopup.jsx
@@ -1,14 +1,34 @@
 import PropTypes from 'prop-types';
+import { FaBars } from 'react-icons/fa6';
+
+import profilePic from '../../assets/images/wpSeemol.jpg';
 
 const MainWindowPopup = ({ isClickWindowBtn }) => {
     return (
         <div
             className={`${
                 isClickWindowBtn
-                    ? 'h-[40rem] origin-bottom p-3 pb-0 duration-500'
+                    ? 'h-[40rem] origin-bottom duration-500'
                     : 'h-0 origin-bottom duration-100'
             } fixed bottom-[2.59rem] left-0 w-[40.56rem]  dark:bg-[#3C3C3C]  `}>
-            {isClickWindowBtn && <div>this is some</div>}
+            {isClickWindowBtn && (
+                <div className="flex h-full">
+                    <div className=" flex flex-col justify-between items-center h-full">
+                        <div className="p-3 text-xl">
+                            <FaBars />
+                        </div>
+                        <div>
+                            <div>
+                                <figure className="w-8 h-8 rounded-full overflow-hidden">
+                                    <img src={profilePic} alt="Profile pic" />
+                                </figure>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="border border-red-500"></div>
+                    <div className="border border-green-400"></div>
+                </div>
+            )}
         </div>
     );
 };
